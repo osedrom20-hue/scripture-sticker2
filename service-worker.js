49c64a1155c18fb5403cce6142bf5e267b5de969
osedrom20-hue <osedrom20@gmail.com>
2026-07-02 18:54:26 -0300

    unomas

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = "scripture-stickers-v2";
+const CACHE_NAME = "scripture-stickers-v1";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./stickers/arrependimento.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
-  self.skipWaiting();
-
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
@@ -21,26 +22,15 @@ self.addEventListener("activate", (event) => {
     caches.keys().then((keys) =>
       Promise.all(keys.map((key) => {
         if (key !== CACHE_NAME) return caches.delete(key);
-        return null;
       }))
-    ).then(() => self.clients.claim())
+    )
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
   event.respondWith(
-    fetch(event.request)
-      .then((networkResponse) => {
-        const responseCopy = networkResponse.clone();
-
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseCopy);
-        });
-
-        return networkResponse;
-      })
-      .catch(() => caches.match(event.request))
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
   );
 });
